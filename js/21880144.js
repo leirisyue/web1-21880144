0fbbd747f456600ae71deb76c32244eafe73703e
me <leirisyue@gmail.com>
2022-10-22 16:44:35 +0700

    done blogs

--- js/21880144.js
+++ js/21880144.js
@@ -10,3 +10,16 @@ async function loadData(request, templateId, viewId) {
    var view = document.getElementById(viewId);
    view.innerHTML = template(context);
 }
+
+async function loadDataJson(request, templateId, viewId,currentPage=1) {
+   const response = await fetch(`${API}/${request}?page=${currentPage}`)
+   const context = await response.json();
+   context.currentPage = currentPage;
+   context.request = request;
+   var source = document.getElementById(templateId).innerHTML;
+   var template = Handlebars.compile(source);
+   var view = document.getElementById(viewId);
+   view.innerHTML = template(context);
+}
+
+
