f9ce140a2ebc9d605b737b34dc2029d59c46a816
me <leirisyue@gmail.com>
2022-10-23 00:13:42 +0700

    done login logout

--- js/21880144.js
+++ js/21880144.js
@@ -24,17 +24,81 @@ async function loadDataJson(request, templateId, viewId,currentPage=1) {
 }
 
 async function getAuthenticateToken(username, password){
-   let response = await fetch(`${AUTHENTICATE_API/authenticate}`,{
-      mehtod: 'POST',
+   let response = await fetch(`${AUTHENTICATE_API}/authenticate`,{
+      method: 'POST',
       headers: {
          'Content-Type':'application/json',
          'Accept':'application/json'
       },
-      body: Json.stringify({username,password})
+      body: JSON.stringify({username,password})
    });
    let result = await response.json();
    if(response.status == 200){
       return result.token
    } 
    throw new Error(result.message)
+}
+
+async function login(e){
+   e.preventDefault();
+   let username = document.getElementById('username').value;
+   let password = document.getElementById('password').value;
+   document.getElementById('errorMessage').innerHTML = '';
+   try {
+      let token = await getAuthenticateToken(username,password);
+      if(token){
+         localStorage.setItem('token', token);
+         document.getElementsByClassName('btn-close')[0].click();
+         displayControl();
+      }
+   } catch (error) {
+      document.getElementById('errorMessage').innerHTML = error;
+      displayControl(false)
+   }
+}
+
+function displayControl(isLogin = true){
+   let linkLogin = document.getElementsByClassName('linkLogin');
+   let linkLogout = document.getElementsByClassName('linkLogout');
+
+   let displayLogin = 'none';
+   let displayLogout = 'block';
+
+   if(!isLogin){
+      displayLogin = 'block';
+      displayLogout = 'none'
+   }
+
+   for(let i=0;i<2;i++){
+      linkLogin[i].style.display = displayLogin
+      linkLogout[i].style.display = displayLogout
+   }
+}
+
+async function checkLogin(){
+   let isLogin = await verifyToken();
+   displayControl(isLogin);
+}
+
+async function verifyToken(){
+   let token = localStorage.getItem('token');
+   if(token){
+      let response = await fetch(`${AUTHENTICATE_API}/verify`,{
+         method: 'POST',
+         headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json',
+            'Authorization':'Bearer' + token
+         }
+      });
+      if(response.status == 200){
+         return true
+      }
+   }
+   return false
+}
+
+function logout(){
+   localStorage.clear();
+   displayControl(false);
 }
