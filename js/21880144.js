1c874a4ea512ce853c1fae9f3d10064a8e5c6136
me <leirisyue@gmail.com>
2022-10-24 21:44:03 +0700

    update

--- js/21880144.js
+++ js/21880144.js
@@ -88,12 +88,12 @@ async function checkLogin(){
 async function verifyToken(){
    let token = localStorage.getItem('token');
    if(token){
-      let response = await fetch(`${AUTHENTICATE_API}/verify`,{
+      let response = await fetch(`${AUTHENTICATE_API}/verify`, {
          method: 'POST',
          headers:{
-            'Content-Type':'application/json',
-            'Accept':'application/json',
-            'Authorization':'Bearer' + token
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "Authorization": 'Bearer ' + token
          },
       });
       if(response.status == 200){
