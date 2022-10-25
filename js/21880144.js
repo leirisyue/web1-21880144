ac0e6550edf86cba1f35d0a0c476cb0868490c2b
me <leirisyue@gmail.com>
2022-10-25 21:47:05 +0700

    UPDATE

--- js/21880144.js
+++ js/21880144.js
@@ -2,15 +2,15 @@
 const API = 'https://web1-api.herokuapp.com/api';
 const AUTHENTICATE_API = 'https://web1-api.herokuapp.com/users';
 
-// async function loadData(request, templateId, viewId) {
-//    const response = await fetch(`${API}/${request}`)
-//    const data = await response.json();
-//    var source = document.getElementById(templateId).innerHTML;
-//    var template = Handlebars.compile(source);
-//    var context = { data:data };
-//    var view = document.getElementById(viewId);
-//    view.innerHTML = template(context);
-// }
+async function loadDataBlog(request, templateId, viewId) {
+   const response = await fetch(`${API}/${request}`)
+   const data = await response.json();
+   var source = document.getElementById(templateId).innerHTML;
+   var template = Handlebars.compile(source);
+   var context = { data:data };
+   var view = document.getElementById(viewId);
+   view.innerHTML = template(context);
+}
 
 async function loadData(request, templateId, viewId) {
    const response = await fetch(`${API}/${request}`)
